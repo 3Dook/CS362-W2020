f9146be7dcb5bf28099eba1bd2c34ff0f983f3c7
Duc <doandu@oregonstate.edu>
2020-02-20 12:53:45 -0800

    Create unittest_classroom_manager.py
    setUp unit testing

---
+++ projects/doandu/Assignment4/unittest_classroom_manager.py
@@ -0,0 +1,33 @@
+import classroom_manager
+
+
+class TestStudent:
+    def test_properties(self):
+        assert True
+    def test_constructor(self):
+        assert True
+    def test_get_full_name(self):
+        assert True
+
+    def test_submit_assignment(self):
+        assert True
+
+    def test_get_assignments(self):
+        assert True
+
+    def test_get_assignment(self):
+        assert True
+
+    def test_get_average(self):
+        assert True
+
+    def test_remove_assignment(self):
+        assert True
+
+class TestAssignment:
+    def test_properties(self):
+        assert True
+    def test_constructor(self):
+        assert True
+    def test_assign_grade(self):
+        assert True
